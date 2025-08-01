d811b880fba896525e983bd7000c565235b0afe1
NaveenAgarwal2004 <naveenagarwal7624@gmail.com>
2025-08-01 22:38:31 +0530

    true

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}`;
-
+const API = `${BACKEND_URL}/api`;
 // Create axios instance with default config
 const apiClient = axios.create({
   baseURL: API,
